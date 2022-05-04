14ed64b6a6cb56bf107839d874254815b6c8e55d
kdy1242 <dy12311229@gmail.com>
2022-05-04 22:37:47 +0900

    평균 구하기

---
+++ src/com/example/test01.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class test01 {
+    public double solution(int[] arr) {
+        double answer = 0;
+        double sum=0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        answer = sum/arr.length;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        test01 t1 = new test01();
+        System.out.println(t1.solution(new int[]{12, 2, 3, 5}));
+    }
+}
